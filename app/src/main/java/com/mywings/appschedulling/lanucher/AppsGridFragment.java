77fbb32e62a4c7e394abe3478c05d0a8b2218315
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-04-02 23:42:28 +0530

    Initial commit

---
+++ app/src/main/java/com/mywings/appschedulling/lanucher/AppsGridFragment.java
@@ -0,0 +1,69 @@
+
+package com.mywings.appschedulling.lanucher;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.View;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+
+public class AppsGridFragment extends GridFragment implements
+        LoaderManager.LoaderCallbacks<ArrayList<AppModel>> {
+
+    AppListAdapter mAdapter;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setEmptyText("No Applications");
+        mAdapter = new AppListAdapter(getActivity());
+        setGridAdapter(mAdapter);
+        setGridShown(false);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<ArrayList<AppModel>> onCreateLoader(int id, Bundle bundle) {
+        return new AppsLoader(getActivity());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<AppModel>> loader,
+                               ArrayList<AppModel> apps) {
+        mAdapter.setData(apps);
+
+        if (isResumed()) {
+            setGridShown(true);
+        } else {
+            setGridShownNoAnimation(true);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<AppModel>> loader) {
+        mAdapter.setData(null);
+    }
+
+    @Override
+    public void onGridItemClick(GridView g, View v, int position, long id) {
+
+        final AppModel app = (AppModel) getGridAdapter().getItem(position);
+
+        if (app != null) {
+            Intent intent = getActivity().getPackageManager()
+                    .getLaunchIntentForPackage(
+                            app.getApplicationPackageName());
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            if (intent != null) {
+                startActivity(intent);
+            }
+
+        }
+    }
+
+
+}
