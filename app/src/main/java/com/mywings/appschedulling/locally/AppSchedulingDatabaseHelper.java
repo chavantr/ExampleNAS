5aea75e0b32d62968967aacf0dc2ab4619d9ac83
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-04-06 11:02:41 +0530

    initial

--- app/src/main/java/com/mywings/appschedulling/locally/AppSchedulingDatabaseHelper.java
+++ app/src/main/java/com/mywings/appschedulling/locally/AppSchedulingDatabaseHelper.java
@@ -7,6 +7,9 @@ import android.support.annotation.Nullable;
 
 public class AppSchedulingDatabaseHelper extends SQLiteOpenHelper {
 
+    //id, name,packagename,localdirectory,numoffiles,size,show,
+
+    private final String CREATE_APP_METADATA = "CREATE TABLE IF NOT EXISTS APP_METADATA";
 
     public AppSchedulingDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
