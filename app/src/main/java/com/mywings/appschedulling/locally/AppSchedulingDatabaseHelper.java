3d2974db0fc5c83fcbb8963fa19d76ff1ca698b3
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-04-07 07:51:19 +0530

    initial

--- app/src/main/java/com/mywings/appschedulling/locally/AppSchedulingDatabaseHelper.java
+++ app/src/main/java/com/mywings/appschedulling/locally/AppSchedulingDatabaseHelper.java
@@ -9,19 +9,21 @@ public class AppSchedulingDatabaseHelper extends SQLiteOpenHelper {
 
     //id, name,packagename,localdirectory,numoffiles,size,show,
 
-    private final String CREATE_APP_METADATA = "CREATE TABLE IF NOT EXISTS APP_METADATA";
+    private final String CREATE_APP_METADATA = "CREATE TABLE IF NOT EXISTS APP_METADATA(id INTEGER PRIMARY KEY AUTOINCREMENT,name text,packagename text,localdirectory text,numoffiles INTEGER,size INTEGER,show text)";
 
     public AppSchedulingDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_APP_METADATA);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
 
     }
+
+
 }
---
+++ app/src/main/java/com/mywings/appschedulling/locally/DbHelper.kt
@@ -0,0 +1,6 @@
+package com.mywings.appschedulling.locally
+
+object DbHelper {
+    const val DB_NAME = ""
+    const val DB_VERSION = 1
+}
